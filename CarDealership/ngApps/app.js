5201955c18c7fb79bf1e22c0e477eabdc4b7d33d
Alex Underwood <Testermonkey@hotmail.com>
2015-06-24 21:35:22 -0700

    added addCar, editCar, deleteCar plus partial views for each

--- CarDealership/ngApps/app.js
+++ CarDealership/ngApps/app.js
@@ -1,5 +1,27 @@
 ﻿(function () {
 
-    angular.module('CarApp', ['ngResource']).constant('CAR_API', '/api/cars/:id');
+    angular.module('CarApp', ['ngResource']).constant('CAR_API', '/api/cars/:id').config(function ($routeProvider) {
+
+    demoApp.config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'CarListController',
+            templateUrl: 'Home/Index.html'
+        })
+        .when('/AddCar', {
+            controller: 'CarListController',
+            templateUrl: 'Partials/addCar.html'
+        })
+        .otherwise({ redirectTo: '/' });
+    });
+
+
+
+
+
+    });
+
+
+
 
 })();
--- CarDealership/ngApps/controllers.js
+++ CarDealership/ngApps/controllers.js
@@ -2,13 +2,39 @@
 
     angular.module('CarApp').controller('CarListController', function (CAR_API, $resource) {
         var self = this;
-
-
         var Car = $resource(CAR_API);
-
         self.cars = Car.query();
 
-        
+        self.addCar = function () {
+            var newCar = new Car({
+                Make: self.newCar.make,
+                Model: self.newCar.model,
+                Price: self.newCar.price,
+                Picture: self.newCar.picture,
+                BriefDescription: self.newCar.briefDescription,
+                FullDescription: self.newCar.fullDescription
+            });
+            newCar.$save(function (result) {
+                self.cars.push(result);
+            });
+        }
 
+        self.editCar = function (origional) {
+            origional.make = car.make;
+            origional.model = car.model;
+            origional.price = car.price;
+            origional.picture = car.picture;
+            origional.briefDescription = car.briefDescription;
+            origional.fullDescription = car.fullDescription;
+            origional.$save();
+        }
+
+        self.deleteCar = function (origional) {
+            origional.$remove({ id: origional.id }, function () {
+                self.cars = self.cars.filter(function (item) {
+                    return origional.id != item.id;
+                });
+            })
+        }
     });
 })();
